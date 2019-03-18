de80a0748b299ad1c5e2a54de2279b26cb9ecea9
Daniel Santos <danielsc.let@gmail.com>
2019-03-18 16:17:06 -0300

    table.scss file formatted / list.scss file removed temporarily / added more funcs in gulpfile.js (build)

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 
 const { src, dest, watch } = require('gulp')
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const rename = require("gulp-rename")
+
 sass.compiler = require('node-sass')
  
 function sassFunc() {
@@ -12,7 +16,23 @@ function sassFunc() {
 
 function watchSass() {
     watch('./scss/*.scss', sassFunc)
-}  
+}
+
+function buildFunc() {
+    return src('./scss/simplify.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(dest('dest/'))
+}
 
 exports.sass = sassFunc
+exports.build = buildFunc
 exports.watchSass = watchSass
